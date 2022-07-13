65aa4b83a48d2330d52de7dfcea1d52f9b762c76
KJH <stonehead334@gmail.com>
2022-07-13 10:24:52 +0900

    HttpServletResponse - 기본 사용법

--- src/main/java/Hi/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/Hi/servlet/basic/request/RequestBodyJsonServlet.java
@@ -25,6 +25,7 @@ public class RequestBodyJsonServlet extends HttpServlet {
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 
         System.out.println("messageBody = " + messageBody);
+        res.getWriter().write("ok");
 
         HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
         System.out.println("helloData.username = " + helloData.getUsername());
