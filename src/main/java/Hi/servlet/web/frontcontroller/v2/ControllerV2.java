602221dd21a428dd2d73c00cf795a5afb92ccf70
KJH <stonehead334@gmail.com>
2022-07-13 16:24:32 +0900

    View 분리 - v2

---
+++ src/main/java/Hi/servlet/web/frontcontroller/v2/ControllerV2.java
@@ -0,0 +1,13 @@
+package Hi.servlet.web.frontcontroller.v2;
+
+import Hi.servlet.web.frontcontroller.MyView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface ControllerV2 {
+
+    MyView process(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException;
+}
---
+++ src/main/java/Hi/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -0,0 +1,17 @@
+package Hi.servlet.web.frontcontroller.v2.controller;
+
+import Hi.servlet.web.frontcontroller.MyView;
+import Hi.servlet.web.frontcontroller.v2.ControllerV2;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberFormControllerV2 implements ControllerV2 {
+
+    @Override
+    public MyView process(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        return new MyView("/WEB-INF/views/new-form.jsp");
+    }
+}
